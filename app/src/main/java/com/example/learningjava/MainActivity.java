1a2177a0d1a1bd125c994a608dc47c07319509c4
Unrealrojo234 <otienotyan812@gmail.com>
2024-10-16 12:23:36 +0300

    First time commit

---
+++ app/src/main/java/com/example/learningjava/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.learningjava;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button msg = findViewById(R.id.msg);
+
+        msg.setOnClickListener(view -> {
+            int rand = ((int) (Math.random()*11));
+            String num = Integer.toString(rand);
+            Toast.makeText(this, num, Toast.LENGTH_LONG).show();
+        });
+    }
+
+}
